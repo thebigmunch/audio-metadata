1d6b35d314cd5df3d18f71f12792d5168e616f1b
thebigmunch <mail@thebigmunch.me>
2020-01-31 18:40:51 -0500

    Update RIFFTags test

--- tests/test_formats_wav.py
+++ tests/test_formats_wav.py
@@ -8,9 +8,31 @@ from audio_metadata import (
 )
 
 
-def test_RIFFTags_invalid():
+def test_RIFFTags():
 	with pytest.raises(InvalidChunk):
 		RIFFTags.load(b'NOTINFO')
+
+	riff_tags_init = RIFFTags(
+		album=['test-album'],
+		artist=['test-artist'],
+		comment=['test-comment'],
+		date=['2000'],
+		genre=['test-genre'],
+		title=['test-title'],
+		tracknumber=['1'],
+	)
+
+	riff_tags_load = RIFFTags.load(
+		b'INFOIPRD\x0b\x00\x00\x00test-album\x00\x00'
+		b'IART\x0c\x00\x00\x00test-artist\x00'
+		b'IGNR\x0b\x00\x00\x00test-genre\x00\x00'
+		b'ICMT\r\x00\x00\x00test-comment\x00\x00'
+		b'INAM\x0b\x00\x00\x00test-title\x00\x00'
+		b'ITRK\x02\x00\x00\x001\x00'
+		b'ICRD\x05\x00\x00\x002000\x00\x00'
+	)
+
+	assert riff_tags_init == riff_tags_load
 
 
 def test_WAV_invalid_header():
